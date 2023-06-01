eba74786b95ddeb56392ec778f2419b4892ef429
Sumit Motghare <smotghare@entrata.com>
2023-06-01 15:44:33 +0530

    No need of replace function

--- find-code-owners.py
+++ find-code-owners.py
@@ -1,6 +1,6 @@
 import sys
 from codeowners import CodeOwners
-filepath = sys.argv[1].replace('/var/www/Entrata/', '')
+filepath = sys.argv[1]
 fileContents = open('CODEOWNERS', "r")
 codeowners = CodeOwners(fileContents.read())
 print(codeowners.of(filepath))
